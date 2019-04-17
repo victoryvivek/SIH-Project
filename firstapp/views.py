386e72af4d10990dda7da7ab37a8146f154b00ea
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2019-04-17 16:51:55 +0530

    Organisation Form working

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import *
 from .forms import OrganisationInfoForm
 
 def startupRegistration(request):
     if request.method=='POST':
         form = OrganisationInfoForm(request.POST)
         if form.is_valid():
+            form.save(commit=True);
             return render(request, 'org_registration.html', {'form': form})
     else:
         form=OrganisationInfoForm()
